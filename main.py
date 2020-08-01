0b0778180608e4fd0efa6f49b80c85997aa910f8
Priultimus <libertsxion@gmail.com>
2020-08-01 15:53:23 -0400

    Change prefix + forbid non devs from using commands in dms

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import asyncio
 import discord
 
 from struc import Silver
-from utils.env import TOKEN
+from utils.env import TOKEN, MAINTAINERS
 
 
 def main():
-    bot = Silver(command_prefix="!")
+    bot = Silver(command_prefix=discord.ext.commands.when_mentioned_or("AG?"))
 
     @bot.check
     async def command_permissions(ctx):
         if not isinstance(ctx.author, discord.Member):
-            return True
+            return True if ctx.author.id in MAINTAINERS else False
 
         perm_level = await bot.permissions.get_cmd_permission(ctx.command, ctx.guild)
         if await bot.permissions.has_permission(ctx.author, perm_level):
